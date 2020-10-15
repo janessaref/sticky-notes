75d0cb8c243f899a4057b07493b0fa2d677eef21
Game Over <janessarefong@gmail.com>
2020-10-14 17:43:00 -0700

    add required modules

--- server.js
+++ server.js
@@ -7,3 +7,17 @@
 6. DELETE api/notes/:id -- query parameter containing unique id of note to delete 
 7. need the notes to rewrite in the db.json
 */
+
+// dependencies
+let express = require("express");
+let path = require("path");
+let fs = require("fs");
+
+
+// sets up the express app telling node we are creating an express server
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+// sets up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
