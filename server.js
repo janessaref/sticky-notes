923a804611ce6731cb52eeb3c556dea0ac442781
Game Over <janessarefong@gmail.com>
2020-10-14 18:28:50 -0700

    change * into / and add static

--- server.js
+++ server.js
@@ -16,8 +16,25 @@ let fs = require("fs");
 
 // sets up the express app telling node we are creating an express server
 let app = express();
-let PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 8080;
 
 // sets up express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+
+//  code below is when users visit another page or click a button
+// If no matching route is found default to home
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// listener to start the server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
