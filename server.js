d75e615a900fc88457cf472e9bd4d32bc8ab26ae
Game Over <janessarefong@gmail.com>
2020-10-14 17:37:12 -0700

    did npm install and pseudocode in server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+/*
+1. require express library and fs
+2. create PORT 
+3. create routes GET notes and *
+4. GET API routes: /api/notes -- should read all db.json and show all saved notes using JSON
+5. POST /api/notes -- saved on req.body and add to db.json and return response to client
+6. DELETE api/notes/:id -- query parameter containing unique id of note to delete 
+7. need the notes to rewrite in the db.json
+*/
